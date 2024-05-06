396778727d3a075c58623c54b1a4ae84d95e1443
Thomas Aldaitz <taldaitz@dawan.fr>
2024-05-06 14:16:16 +0200

    Add a login page in the project

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+    <form action="" method="post">
+        <input type="text" name="login" id="login">
+        <input type="password" name="password" id="password">
+
+        <input type="submit" value="OK">
+    </form>
+    
+</body>
+</html>
---
+++ src/traitement.php
@@ -0,0 +1,5 @@
+<?php
+
+function traitement() {
+    echo 'Traitement';
+}
